74e70e29b0670eba6647ad2ca92fbd1cd1b78224
Kalmikov Sergey <KalmikovW13@gmail.com>
2019-09-07 18:15:22 -0500

    v1

---
+++ HSProject1/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  HSProject1
+//
+//  Created by Сергей Калмыков on 9/7/19.
+//  Copyright © 2019 Сергей Калмыков. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ HSProject1/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  HSProject1
+//
+//  Created by Сергей Калмыков on 9/7/19.
+//  Copyright © 2019 Сергей Калмыков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var pictures = [String]()
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+    }
+    
+}
