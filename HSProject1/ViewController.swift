03121db2f336690609ad0167e9e7385a76e1116b
Kalmikov Sergey <KalmikovW13@gmail.com>
2019-09-08 21:38:43 -0500

    v1.3

--- HSProject1/ViewController.swift
+++ HSProject1/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     var pictures = [String]()
     //
     override func viewDidLoad() {
@@ -16,16 +16,27 @@ class ViewController: UIViewController {
         
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
+        print(path)
         let items = try! fm.contentsOfDirectory(atPath: path)
-        
+      print(items)
         for item in items {
+            print(item)
             if item.hasPrefix("nssl"){
                 pictures.append(item)
-                print("1")
+                
             }
         }
         
         print(pictures)
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 }
